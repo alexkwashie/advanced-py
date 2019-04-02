d560451190ba5f946dd62d52cda1602233585fbe
Alex A. Kwashie <alex.kwashie@hotmail.com>
2019-04-02 10:20:26 +0100

    Working with methods etc

---
+++ index.py
@@ -0,0 +1,42 @@
+import random
+
+
+def ask_number():
+    u_input = int(input("Guess the number"))
+    if u_input in magic_numbers:
+        print("You are in!")
+    else:
+        print("You are out")
+
+
+
+magic_numbers= [3,8]
+
+chances = 4
+def loop_num(i):
+    for ie in range(i):
+        print("This is you  number {}".format(ie))
+        ask_number()
+
+loop_num(7);
+
+
+
+print("This is then End")
+
+
+
+
+
+
+
+# min = 100
+
+# for i in range(10):
+#     ran = random.randint(0,100)
+#     print(ran)
+#     if ran <= min:
+#         min = ran
+
+# print(min)
+
